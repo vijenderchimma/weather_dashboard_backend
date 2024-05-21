eed06c4dd156d5e9464f56c0602819595adabe9e
vijender chimma <vijenderchimma424@gmail.com>
2024-05-21 12:08:44 +0530

    added header for openstretmap

--- index.js
+++ index.js
@@ -59,6 +59,9 @@ app.post('/name', async (req, res) => {
                 q: searchQuery,
                 format: 'json',
                 limit: 1
+            },
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
             }
         });
 
