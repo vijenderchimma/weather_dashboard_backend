ce3f657e3a28687a4746c7040e6b7c3248c46930
vijender chimma <vijenderchimma424@gmail.com>
2024-05-21 10:35:54 +0530

    removed unwanted files

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ app.get('/weather/daily', async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
